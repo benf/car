4a1b12489dcde41f93c461274b31bbbe86cf0cea
ben <ben@notebook.franzke.lan>
2010-01-02 17:04:56 +0100

    added some comments

--- empfaenger.c
+++ empfaenger.c
@@ -97,11 +97,10 @@ ISR (INT0_vect) {
 		// parameter for command
 		case 1:
 			param = data;
-	//		break;
-		// checksum
-	//	case 2:
-	//		if (data == _crc_ibutton_update(_crc_ibutton_update(0, action), param))
-			cmd(action,param);
+			//break;
+		//case 2: // checksum
+			//if (data == _crc_ibutton_update(_crc_ibutton_update(0, action), param))
+				cmd(action,param);
 			id = 0;
 			RFXX_WRT_CMD(0xCA81); // reset fifo
 			RFXX_WRT_CMD(0xCA83); //  - || -
@@ -115,7 +114,6 @@ ISR (INT0_vect) {
 
 int main(void)
 {
-//	_delay_ms(500);
 	rfxx_init();
 	rf12_init(0);
 
@@ -125,66 +123,69 @@ int main(void)
 
 	RFXX_nIRQ_PORT  &= ~(1 << RFXX_nIRQ);
 
-//	GICR  = (1 << INT0);
-	//	MCUCR = (1 << ISC01) | (1 << ISC00);
+	// enable external interrupt 0
+	//GICR  = (1 << INT0);
+	
+	//not used
+	/////MCUCR = (1 << ISC01) | (1 << ISC00);
 
-//	sei();
+	// enable interrupts (global)
+	//sei();
+
 
 	DDRA = 0xff;
 
 	DDRC  |=  (1 << DDC5);
 	PORTC |=  (1 << PC5);
 
+	
 	DDR_ENGINE |= (1 << ENGINE_LEFT) | (1 << ENGINE_RIGHT) | (1 << ENGINE_ENABLE);
 	PORT_ENGINE &= ~(1 << ENGINE_ENABLE);
 
-  DDR_DIRECTION |= (1 << DIR_LEFT) | (1 << DIR_RIGHT) | (1 << DIR_EN);
+
+	DDR_DIRECTION |= (1 << DIR_LEFT) | (1 << DIR_RIGHT) | (1 << DIR_EN);
 	DIRECTION &= ~(1 << DIR_EN);
 
 
+	// Interrupt PIN = Input
 	RFXX_nIRQ_PORT &= ~(1 << RFXX_nIRQ);
 
+	// enable receiver's FIFO
 	RFXX_WRT_CMD(0xCA83);
 
+	// PWM configuration
 	OCR1A   = 0;
 	TCCR1A  =  (1 << COM1A1) | (1 << WGM12) | (1 << WGM11) | (1 << WGM10);
 	TCCR1B  =  (1 << CS10);
 
-
-
-//	while(1);
-
-
 	while (1) {
 		while (RFXX_nIRQ_PIN & (1 << RFXX_nIRQ));
 
-	PORTC ^= (1 << PC2);
+		PORTC ^= (1 << PC2);
 
-	uint8_t data = rf12_recv();
+		uint8_t data = rf12_recv();
 
-	PORTC &= ~(1 << PC3);
-	PORTC &= ~(1 << PC4);
+		PORTC &= ~(1 << PC3);
+		PORTC &= ~(1 << PC4);
 
-	switch (id) {
-		case 0:
-			action = data;
-			id = 1;
-			PORTC |= (1 << PC3);
-			break;
-		// parameter for command
-		case 1:
-			param = data;
-	//		break;
-		// checksum
-	//	case 2:
-	//		if (data == _crc_ibutton_update(_crc_ibutton_update(0, action), param))
-			cmd(action,param);
-			id = 0;
-			RFXX_WRT_CMD(0xCA81); // reset fifo
-			RFXX_WRT_CMD(0xCA83); //  - || -
-			PORTC |= (1 << PC4);
-
-			break;
-	}
+		switch (id) {
+			case 0:
+				action = data;
+				id = 1;
+				PORTC |= (1 << PC3);
+				break;
+			// parameter for command
+			case 1:
+				param = data;
+				//break;
+			//case 2:	// checksum
+			//	if (data == _crc_ibutton_update(_crc_ibutton_update(0, action), param))
+					cmd(action,param);
+				id = 0;
+				RFXX_WRT_CMD(0xCA81); // reset fifo
+				RFXX_WRT_CMD(0xCA83); //  - || -
+				PORTC |= (1 << PC4);
+				break;
+		}
 	}
 }
