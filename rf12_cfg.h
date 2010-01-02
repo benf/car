084d6ba8263f2104404a6f3f2ab26af1a0793ce8
ben <ben@notebook.franzke.lan>
2010-01-02 16:42:32 +0100

    header files

---
+++ rf12_cfg.h
@@ -0,0 +1,14 @@
+#define SOFT_SPI   1
+
+#define RFXX_nIRQ_PORT PORTD
+#define RFXX_nIRQ_PIN  PIND
+#define RFXX_nIRQ      PD2
+#define RFXX_FSK       PB1
+
+#define PORT_SPI   PORTB
+#define PIN_SPI    PINB
+#define DDR_SPI    DDRB
+#define SPI_SS     PB4
+#define SPI_MOSI   PB5
+#define SPI_MISO   PB6
+#define SPI_SCK    PB7
