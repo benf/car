5c9bed742b271f29f3ef755b3743e47066d02f29
ben <benjaminfranzke@gmail.com>
2010-01-16 14:45:31 +0100

    sender: added crc checksum

--- sender.c
+++ sender.c
@@ -1,4 +1,5 @@
-#define F_CPU 4000000UL
+#include "usart_cfg.h"
+#include "usart.h"
 
 #include <stdint.h>
 
@@ -10,8 +11,6 @@
 
 #include "rf12_cfg.h"
 #include "rfxx.h"
-
-#include "usart.h"
 
 uint8_t buffer[4];
 
@@ -29,34 +28,35 @@ volatile uint8_t queue = 0;
  * the RS-232 interface also uses
  */
 ISR (USART_RXC_vect) {
+	cli();
 	++id;
 	tmp = UDR;
 
 	if (tmp == 0xAA)
 		id = 0;
-	else
+	else {
 		buffer[id] = tmp;
+	}
 
 	if (id == 2) {
-		// TODO: add crc checksum and check it in empfaenger.c
+		buffer[3] = _crc_ibutton_update(_crc_ibutton_update(0, buffer[1]), buffer[2]);
+		rf12_send_data(buffer + 1, 3);
 
-		rf12_send_data(buffer + 1, 2);
 		PORTC ^= (1 << PC0);
 	}
 	PORTC ^= (1 << PC1);
+	sei();
 }
 
 int main(void) {
-	//uint8_t i, ChkSum;
-
-	sei();
-
 	// for debugging purposes
 	DDRC  =  0xff;
 
 	init_usart();
+	sei();
 	// wait 200ms for POR initialization (see empfaenger.c for further information)
 	_delay_ms(200);
+
 	rfxx_init();
 	// TODO: replace 0/1 with understandable constants for RX/TX selection
 	// 1 = transfer mode, 0 = receive mode
@@ -64,55 +64,9 @@ int main(void) {
 
 	DDRD &= ~(1 << RFXX_nIRQ);
 
-	while (1);
+	while(1);
+
 	return 0;
-	/* TODO: Do we need any of the following code?
-
-	ChkSum = 0;
-	uint8_t data[17];
-	for (i = 0; i < 16; ++i) {
-		data[i] = 0x30 + i;
-		ChkSum += data[i];
-	}
-	data[16] = ChkSum;
-
-	while (1) {	
-
-		PORTC |=  (1 << PC0);
-		rf12_send_data(data, 17);
-		PORTC &= ~(1 << PC0);
-		for (i = 0; i < 20; ++i)
-			_delay_ms(100);
-	}
-	*/
-	
-	/*
-		tmp = id_saved;
-		if (tmp > id_send) { 
-			rf02_send_data(MEINBUFFER + id_send, tmp-id_send);
-			id_send = tmp;
-
-		  // integer overflow occured
-		} else if (tmp < id_send) {
-			rf02_send_data(MEINBUFFER + id_send, 256-id_send);
-			id_send = 0;
-		}
-	}
-*/
-	//	rf02_send_data((uint8_t*)"hallo, was geht", 15);
-
-
-	//while (1);
-	/*{
-		PORTC |= (1 << PC0);
-		rf02_send_data(data, 17);
-		PORTC &= ~(1 << PC0);
-
-		for (i = 0; i < 200; ++i)
-		  _delay_ms(10);
-	}
-	*/
-	//return 0;
 }
 
 /* vim: set sts=0 fenc=utf-8: */
