23fde224edb769f752c53bc72640271f3be3f40f
ben <benjaminfranzke@gmail.com>
2010-01-16 14:34:22 +0100

    usart: change baud to 4800

--- usart_cfg.h
+++ usart_cfg.h
@@ -1,7 +1,14 @@
-#define F_CPU 4000000UL
-#define BAUD 9600UL
+#ifndef _USART_CFG_H_
+#define _USART_CFG_H_
+
+//#define F_CPU 4000000UL
+#define F_CPU 14745600UL
+
+//#define BAUD 9600UL
+#define BAUD 4800UL
 #include <util/setbaud.h>
 
 
 // FUSE-FLAGS for external quarz
 // -U lfuse:w:0xee:m -U hfuse:w:0x99:m 
+#endif // _USART_CFG_H_
