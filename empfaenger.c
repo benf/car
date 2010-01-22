5eabda2954ad477b111836cd67a95c2b87ff5fd0
benjamin <benjamin@netdirekt.(none)>
2010-01-22 10:49:19 +0100

    empfaenger: fit sensoric to existing coding style

--- empfaenger.c
+++ empfaenger.c
@@ -38,11 +38,9 @@ volatile uint8_t rwd;
 #define DIR_EN        PA2
 
 
-void init_special()
-{
+void init_special() {
 	DDRA &= ~((1 << PA3) & (1 << PA4));
 
-//  TCCR0 |= (1 << CS00 ) | ( 1 << CS02 );
 	TCCR0 |= (1 << CS02);
 	TIMSK |= (1 << TOIE0 );
   
@@ -51,41 +49,43 @@ void init_special()
 }
 uint16_t ReadADC(uint8_t channel)
 {
-  uint8_t i;
-  uint16_t result;
+	uint8_t i;
+	uint16_t result;
  
-  ADMUX = channel;                      
-  ADMUX |= (1<<REFS1) | (1<<REFS0); // Vref = 2.56V
-  ADCSRA = (1<<ADEN) | (1<<ADPS2) | (1<<ADPS0);    // Frequenzteiler 32
-  ADCSRA |= (1<<ADSC);
-  while ( ADCSRA & (1<<ADSC) ) {}
-  result = ADCW;  
-  result = 0; 
-  //Eigentliche Messung beginnt jetzt
-  for ( i=0; i<4; ++i )
-  {
-    ADCSRA |= (1<<ADSC);            // single conversion
-    while ( ADCSRA & (1<<ADSC) ) {}
-    result += ADCW;	
-  }
-  ADCSRA &= ~(1<<ADEN);
+	ADMUX = channel;                      
+	ADMUX |= (1<<REFS1) | (1<<REFS0); // Vref = 2.56V
+	ADCSRA = (1<<ADEN) | (1<<ADPS2) | (1<<ADPS0);    // Frequenzteiler 32
+
+	// dummy messung
+	ADCSRA |= (1<<ADSC);
+	while ( ADCSRA & (1<<ADSC) ) {}
+	result = ADCW;  
+
+	result = 0; 
+	// Eigentliche Messung beginnt jetzt
+	for ( i=0; i<4; ++i ) {
+		ADCSRA |= (1<<ADSC);            // single conversion
+		while ( ADCSRA & (1<<ADSC) );
+		result += ADCW;	
+	}
+	ADCSRA &= ~(1<<ADEN);
  
-  result /= 4; //Mittelwert und zurück
-  return result;
+	result /= 4; //Mittelwert und zurück
+	return result;
 }
 
 
 ISR (TIMER0_OVF_vect)
 {
 	cli();
-  //Overflow
-  //ADC LESEN
-  uint16_t adcvalue;
-  uint8_t  erg;
+	//Overflow
+	//ADC LESEN
+	uint16_t adcvalue;
+	uint8_t  erg;
 
-  adcvalue = ReadADC(3); //Kanal 3 lesen, SHARP Sensor
-  erg = adcvalue/4;  //Durch 4 teilen => Spannung als 8-Bit Wert mal 100 (ohne Komma von 0-255)
-  /*   	
+	adcvalue = ReadADC(3); //Kanal 3 lesen, SHARP Sensor
+	erg = adcvalue/4;  //Durch 4 teilen => Spannung als 8-Bit Wert mal 100 (ohne Komma von 0-255)
+	/*   	
 		IR-Sensor
 		---------
 
@@ -99,30 +99,26 @@ ISR (TIMER0_OVF_vect)
 		Hell	-	1K Ohm
 		Dunkel	-	500K Ohm
   
-  */
-  if((erg >= 50))  //ca. 30cm
-  {
-	  hinderniss = 1;
-    PORTC |=  (1 << PC3);
-	if (!rwd)
+	*/
+	if ((erg >= 50)) { //ca. 30cm
+		hinderniss = 1;
+		PORTC |=  (1 << PC3);
+		if (!rwd)
 		cmd('S', 0);
-  }else
-  {
-	  hinderniss = 0;
-    PORTC &= ~(1 << PC3);
-  }
+	} else {
+		hinderniss = 0;
+		PORTC &= ~(1 << PC3);
+	}
       
-  adcvalue = ReadADC(4); //Kanal 4 lesen, LDR
-  erg = adcvalue/4; 
-  if((erg >= 130))
-  {
-    PORTC |= (1 << PC0);
-  }else
-  {
-    PORTC &= ~(1 << PC0);
-  }
-  PORTC ^= (1 << PC1);
-  sei();
+	adcvalue = ReadADC(4); //Kanal 4 lesen, LDR
+	erg = adcvalue/4; 
+	if ((erg >= 130))  {
+		PORTC |= (1 << PC0);
+	} else {
+		PORTC &= ~(1 << PC0);
+	}
+	PORTC ^= (1 << PC1);
+	sei();
 }
 
 
