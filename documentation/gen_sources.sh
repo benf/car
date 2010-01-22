51ae598bb528990d0c463bf757b261986c543f8f
ben <benjaminfranzke@gmail.com>
2010-01-22 17:03:30 +0100

    gen_sourc: add begin

--- documentation/gen_sources.sh
+++ documentation/gen_sources.sh
@@ -3,7 +3,8 @@ for file in ../*.{c,h} ../Makefile ../pc/Makefile ../pc/joystick.c
 do
 	name=$(basename $file)
 	echo '\lstset{language=C}' > $name.tex
+	echo '\begin{lstlisting}' > $name.tex
 	cat $file >> $name.tex
-	sed -i 's/[#&]/\\\0/g' $name.tex
+	#sed -i 's/[#&]/\\\0/g' $name.tex
 	echo '\end{lstlisting}' >> $name.tex
 done
