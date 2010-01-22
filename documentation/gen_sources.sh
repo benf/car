9af9608189a486b6a78527a5be6cd37f8ec0fce1
ben <benjaminfranzke@gmail.com>
2010-01-22 16:29:57 +0100

    correct mistake

--- documentation/gen_sources.sh
+++ documentation/gen_sources.sh
@@ -2,7 +2,7 @@
 for file in ../*.{c,h} ../Makefile ../pc/Makefile ../pc/joystick.c
 do
 	name=$(basename $file)
-	echo "\lstset{language=C}" > $name.tex
-	cat $file >> $name.text
-	echo "\end{lstlisting}" >> $name.tex
+	echo '\lstset{language=C}' > $name.tex
+	cat $file >> $name.tex
+	echo '\end{lstlisting}' >> $name.tex
 done
